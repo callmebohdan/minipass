ddf1458148e92a7e1faedbb7ebfd72a0a1e312a4
callmebohdan <bohdan.ponomarenko.work@gmail.com>
2024-09-11 08:40:28 +0300

    Update script for cleaning

--- scripts/clean.bat
+++ scripts/clean.bat
@@ -1,38 +1,29 @@
 @echo off
+
 setlocal
 
-REM Save the current directory
 pushd %~dp0
-
-REM Navigate up one level from the scripts directory
 cd ..
-
-REM Store the current directory (project root) in PROJECT_DIR
 set PROJECT_DIR=%cd%
-
-REM Return to the original directory (scripts directory)
 popd
 
+set BIN_DIR=%PROJECT_DIR%\bin
 set BUILD_DIR=%PROJECT_DIR%\build
 set OUT_DIR=%PROJECT_DIR%\out
-set BIN_DIR=%PROJECT_DIR%\bin
 
-:: Clean previous build if exists
+if exist "%BIN_DIR%" (
+    echo Cleaning bin/ directory...
+    rd /s /q "%BIN_DIR%"
+)
+
 if exist "%BUILD_DIR%" (
-    echo Cleaning previous build...
+    echo Cleaning build/ directory...
     rd /s /q "%BUILD_DIR%"
 )
 
-:: Clean previous build if exists
 if exist "%OUT_DIR%" (
-    echo Cleaning previous build output...
+    echo Cleaning out/ directory...
     rd /s /q "%OUT_DIR%"
-)
-
-:: Clean previous build bin directory if exists
-if exist "%BIN_DIR%" (
-    echo Cleaning previous build bin directory...
-    rd /s /q "%BIN_DIR%"
 )
 
 endlocal
