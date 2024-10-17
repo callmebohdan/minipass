6dd30194fa1c64dcbd0be2518693b62e07a0ba46
callmebohdan <bohdan.ponomarenko.work@gmail.com>
2024-10-17 19:10:38 +0300

    Remove comments; Combine CLI and GUI builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -e
 
-# Set QT_QPA_PLATFORM to xcb for running on xcb
 export QT_QPA_PLATFORM=xcb
 
-# Save the current directory
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
-# Navigate up one level from the scripts directory
 cd "$SCRIPT_DIR/.."
-
-# Store the current directory (project root) in PROJECT_DIR
 PROJECT_DIR=$(pwd)
 
 BUILD_DIR="$PROJECT_DIR/build"
@@ -18,42 +13,35 @@ BIN_DIR="$PROJECT_DIR/bin"
 QT_PATH="/usr/lib/x86_64-linux-gnu/cmake/Qt6"
 QT_LIB_PATH="/usr/lib/x86_64-linux-gnu"
 
-# Clean previous build if exists
-if [ -d "$BUILD_DIR" ]; then
-    echo "Cleaning previous build..."
-    rm -rf "$BUILD_DIR"
-fi
-
 if [ -d "$BIN_DIR" ]; then
     echo "Cleaning previous build bin/..."
     rm -rf "$BIN_DIR"
 fi
 
-# Create build directory
+if [ -d "$BUILD_DIR" ]; then
+    echo "Cleaning previous build..."
+    rm -rf "$BUILD_DIR"
+fi
+
 echo "Creating build directory..."
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
 
-
-# Configure the project with CMake
 echo "Running CMake configuration..."
 cmake -G "Unix Makefiles" -DCMAKE_PREFIX_PATH="$QT_PATH" "$PROJECT_DIR"
 
-# Build the project
 echo "Building the project..."
 make
 
-# Copy the executable and necessary libraries into the bin directory
-if [ -f "$BUILD_DIR/src/minipassGUI" ]; then
+if [ -f "$BUILD_DIR/src/minipass" ]; then
     mkdir -p "$BIN_DIR"
     echo "Copying the application into the bin/ directory..."
-    cp "$BUILD_DIR/src/minipassCLI" "$BIN_DIR"
-    cp "$BUILD_DIR/src/minipassGUI" "$BIN_DIR"
+    cp "$BUILD_DIR/src/" "$BIN_DIR"
     cp "$QT_LIB_PATH/libQt6Core.so.6" "$BIN_DIR"
     cp "$QT_LIB_PATH/libQt6Gui.so.6" "$BIN_DIR"
     cp "$QT_LIB_PATH/libQt6Widgets.so.6" "$BIN_DIR"
 else
-    echo "Error: minipassGUI was not created."
+    echo "Error: minipass was not created."
     exit 1
 fi
 
@@ -64,13 +52,3 @@ else
     echo "Error: tests were not created."
     exit 1
 fi
-
-# Run the executable if it was created
-if [ -f "$BIN_DIR/minipassGUI" ]; then
-    echo "Running the application..."
-    "$BIN_DIR/minipassGUI"
-else
-    echo "Error: minipassGUI was not created."
-    exit 1
-fi
-
