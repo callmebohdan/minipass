699d8e11f9b2e7a06768cef8b0cf599cd3d8ddab
callmebohdan <bohdan.ponomarenko.work@gmail.com>
2024-10-21 00:35:30 +0300

    Add extra cleanups; Generate ui_minipass.h; Copy correct directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -e
 
 export QT_QPA_PLATFORM=xcb
@@ -7,8 +8,11 @@ SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 cd "$SCRIPT_DIR/.."
 PROJECT_DIR=$(pwd)
 
-BUILD_DIR="$PROJECT_DIR/build"
 BIN_DIR="$PROJECT_DIR/bin"
+BUILD_DIR="$PROJECT_DIR/build"
+OUT_DIR="$PROJECT_DIR/out"
+SRC_DIR="$PROJECT_DIR/src"
+
 QT_PATH="/usr/lib/x86_64-linux-gnu/cmake/Qt6"
 QT_LIB_PATH="/usr/lib/x86_64-linux-gnu"
 
@@ -22,6 +26,11 @@ if [ -d "$BUILD_DIR" ]; then
     rm -rf "$BUILD_DIR"
 fi
 
+if [ -d "$OUT_DIR" ]; then
+    echo "Cleaning bin/ directory..."
+    rm -rf "$BIN_DIR"
+fi
+
 echo "Creating build/ directory..."
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
@@ -29,13 +38,16 @@ cd "$BUILD_DIR"
 echo "Running CMake configuration..."
 cmake -G "Unix Makefiles" -DCMAKE_PREFIX_PATH="$QT_PATH" "$PROJECT_DIR"
 
+echo "Generate ui_minipass.h"
+uic "$SRC_DIR/minipass.ui" -o "$SRC_DIR/ui_minipass.h"
+
 echo "Building the project..."
 make
 
-if [ -f "$BUILD_DIR/src/minipass" ]; then
+if [ -f "$BUILD_DIR/minipass" ]; then
     mkdir -p "$BIN_DIR"
     echo "Copying the application into the bin/ directory..."
-    cp "$BUILD_DIR/src/" "$BIN_DIR"
+    cp "$BUILD_DIR/minipass" "$BIN_DIR"
     cp "$QT_LIB_PATH/libQt6Core.so.6" "$BIN_DIR"
     cp "$QT_LIB_PATH/libQt6Gui.so.6" "$BIN_DIR"
     cp "$QT_LIB_PATH/libQt6Widgets.so.6" "$BIN_DIR"
