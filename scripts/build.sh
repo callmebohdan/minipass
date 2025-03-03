83430ce048f8fdf51d7e5d0ffea3ac3680f8360b
callmebohdan <bohdan.ponomarenko.work@gmail.com>
2025-03-03 21:43:13 +0200

    Refactor build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@
 set -e
 
 export QT_QPA_PLATFORM=xcb
+export PATH="/usr/lib/qt6/bin:$PATH"
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 cd "$SCRIPT_DIR/.."
@@ -14,22 +15,11 @@ OUT_DIR="$PROJECT_DIR/out"
 SRC_DIR="$PROJECT_DIR/src"
 
 QT_PATH="/usr/lib/x86_64-linux-gnu/cmake/Qt6"
-QT_LIB_PATH="/usr/lib/x86_64-linux-gnu"
+QT_BIN_PATH="/usr/lib/qt6/bin"
+QT_LIB_PATH="/usr/lib/qt6/libexec"
 
-if [ -d "$BIN_DIR" ]; then
-    echo "Cleaning bin/ directory..."
-    rm -rf "$BIN_DIR"
-fi
-
-if [ -d "$BUILD_DIR" ]; then
-    echo "Cleaning build/ directory..."
-    rm -rf "$BUILD_DIR"
-fi
-
-if [ -d "$OUT_DIR" ]; then
-    echo "Cleaning bin/ directory..."
-    rm -rf "$BIN_DIR"
-fi
+echo "Cleaning directories..."
+rm -rf "$BIN_DIR" "$BUILD_DIR" "$OUT_DIR"
 
 echo "Creating build/ directory..."
 mkdir -p "$BUILD_DIR"
@@ -38,28 +28,22 @@ cd "$BUILD_DIR"
 echo "Running CMake configuration..."
 cmake -G "Unix Makefiles" -DCMAKE_PREFIX_PATH="$QT_PATH" "$PROJECT_DIR"
 
-echo "Generate ui_minipass.h"
-uic "$SRC_DIR/minipass.ui" -o "$SRC_DIR/ui_minipass.h"
+echo "Generating ui_minipass.h..."
+"$QT_LIB_PATH/uic" "$SRC_DIR/minipass.ui" -o "$SRC_DIR/ui_minipass.h"
 
-echo "Building the project..."
-make
-
-if [ -f "$BUILD_DIR/minipass" ]; then
+# Verbose build output
+make VERBOSE=1
+if [ -f "$BUILD_DIR/src/minipass" ]; then
     mkdir -p "$BIN_DIR"
-    echo "Copying the application into the bin/ directory..."
-    cp "$BUILD_DIR/minipass" "$BIN_DIR"
-    cp "$QT_LIB_PATH/libQt6Core.so.6" "$BIN_DIR"
-    cp "$QT_LIB_PATH/libQt6Gui.so.6" "$BIN_DIR"
-    cp "$QT_LIB_PATH/libQt6Widgets.so.6" "$BIN_DIR"
+    echo "Copying the application into bin/..."
+    cp "$BUILD_DIR/src/minipass" "$BIN_DIR"
 else
-    echo "Error: minipass was not created."
-    exit 1
+    echo "Error: minipass build was not successfull."
 fi
 
-if [ -f "$BUILD_DIR/test/TestMinipass" ]; then
-    echo "Copying the tests into the bin/ directory..."
-    cp "$BUILD_DIR/test/TestMinipass" "$BIN_DIR"
-else
-    echo "Error: tests were not created."
-    exit 1
-fi
+# if [ -f "$BUILD_DIR/test/TestMinipass" ]; then
+#     echo "Copying tests into bin/..."
+#     cp "$BUILD_DIR/test/TestMinipass" "$BIN_DIR"
+# else
+#     echo "No tests found. Skipping."
+# fi
