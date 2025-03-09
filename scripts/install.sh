19c4eb284718a72fabebf9105c9db2a19f5884a5
callmebohdan <bohdan.ponomarenko.work@gmail.com>
2025-03-09 21:35:10 +0200

    Fix: Install gtest static libraries

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,11 +18,13 @@ sudo apt-get -y install libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev mesa-comm
 # Install Qt6
 sudo apt-get -y install qt6-base-dev qt6-base-dev-tools qt6-tools-dev qt6-tools-dev-tools qt6ct qt6-multimedia-dev
 
-# Install GTest
-sudo apt-get -y install libgtest-dev
-
-# Build and install static libraries
-cd /usr/src/gtest && sudo cmake . && sudo make && sudo mv lib* /usr/lib/ && cd -
-
 # Install XKB library
 sudo apt-get -y install libxkbcommon-dev
+
+# Install and build GTest static libraries
+sudo apt-get -y install libgtest-dev
+cd /usr/src/gtest
+cmake .
+make
+mv lib/libgtest* /usr/lib/
+cd -
