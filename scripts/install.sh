07436227460f94655a1d0937a21783ab22f0bf48
callmebohdan <bohdan.ponomarenko.work@gmail.com>
2025-03-09 13:40:08 +0200

    Remove empty space

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,7 +16,7 @@ sudo apt-get -y install build-essential
 sudo apt-get -y install libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev libxcb-cursor0
 
 # Install Qt6
-sudo apt-get -y install qt6-base-dev qt6-base-dev-tools  qt6-tools-dev qt6-tools-dev-tools qt6ct qt6-multimedia-dev
+sudo apt-get -y install qt6-base-dev qt6-base-dev-tools qt6-tools-dev qt6-tools-dev-tools qt6ct qt6-multimedia-dev
 
 # Install GTest
 sudo apt-get -y install libgtest-dev
